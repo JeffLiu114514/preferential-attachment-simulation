fa2b0aac5d83a12b319e6020058ae746683aff86
JeffLiu114514 <jeff_jfl@163.com>
2024-02-21 11:53:46 -0500

    notes

--- wrapper/main.py
+++ wrapper/main.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     clique_size_expectation = 5
     n_servers = int(n_nodes * user_server_ratio)
     n_cliques = int(n_servers * server_clique_ratio)
-    posts_hyperparameters = [1, 0.6, 0.2, 0.2, 0.4, 0.6, 0, 0.2, 0.1, 0.1] # alpha1, alpha2, alpha3, beta1, beta2, beta3, engagement_threshold, follow2, follow3, unfollow1
+    
+    posts_hyperparameters = [1, 0.6, 0.2, 0.2, 0.4, 0.6, 0, 0.2, 0.1, 0.1] 
+    # alpha1, alpha2, alpha3, beta1, beta2, beta3, engagement_threshold, follow2, follow3, unfollow1
     
     gw = GenerationWrapper(n_nodes, n_servers, n_cliques, clique_size_expectation, posts_hyperparameters)
     # print(gw.user_visible_by_follow)
